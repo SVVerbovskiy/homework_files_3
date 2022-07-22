0f7b6c8dd52c47d118cacbabcbf37fdd2c0aaeb1
Вербовский Сергей <verbovskii@inbox.ru>
2022-07-22 18:00:06 +0500

    exercise complete

--- main.py
+++ main.py
@@ -9,19 +9,10 @@ list_files = glob.glob(glob_path)
 list_files = sorted(list_files, key=lambda x: os.stat(os.path.join(dir_name, x)).st_size)
 
 for file_name in list_files:
-    print(file_name)
     with open(file_name, 'r', encoding='utf-8') as fr, open(new_file, 'a', encoding='utf-8') as fw:
-        fw.write(f'\n\n------------ {file_name}\n\n')
-        for line in fr:
+        text = fr.read()
+        lines = text.count('\n') + 1
+        fw.write(f'\n{os.path.basename(file_name)}\n{lines}\n')
+        for line in text:
             fw.write(line)
 
-    file_path = os.path.join(dir_name, file_name)
-    file_size = os.stat(file_path).st_size
-    print(file_size, ' -->', file_name)
-
-# if list_files:
-#     for file_name in list_files:
-#         with open(file_name, 'r') as fr, open(new_file, 'a') as fw:
-#             fw.write(f'\n\n------------ {file_name}\n\n')
-#             for line in fr:
-#                 fw.write(line)
