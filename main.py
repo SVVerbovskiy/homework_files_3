84511d2488d515396423afbee75df4bc322db318
Вербовский Сергей <verbovskii@inbox.ru>
2022-07-22 17:51:57 +0500

    sort complete

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+import glob
 import os
 
-print(os.listdir(os.getcwd()))
-with open('1.txt', encoding='utf-8') as f:
-    text = f.read()
-    lines_1 = text.count('\n') + 1
-with open('2.txt', encoding='utf-8') as f:
-    text = f.read()
-    lines_2 = text.count('\n') + 1
-with open('3.txt', encoding='utf-8') as f:
-    text = f.read()
-    lines_3 = text.count('\n') + 1
-print(lines_1, lines_2, lines_3)
+dir_name = 'D:\\Netology\Homeworks Files_3'
+pattern = '*.txt'
+new_file = 'new_file.all'
+glob_path = os.path.join(dir_name, pattern)
+list_files = glob.glob(glob_path)
+list_files = sorted(list_files, key=lambda x: os.stat(os.path.join(dir_name, x)).st_size)
+
+for file_name in list_files:
+    print(file_name)
+    with open(file_name, 'r', encoding='utf-8') as fr, open(new_file, 'a', encoding='utf-8') as fw:
+        fw.write(f'\n\n------------ {file_name}\n\n')
+        for line in fr:
+            fw.write(line)
+
+    file_path = os.path.join(dir_name, file_name)
+    file_size = os.stat(file_path).st_size
+    print(file_size, ' -->', file_name)
+
+# if list_files:
+#     for file_name in list_files:
+#         with open(file_name, 'r') as fr, open(new_file, 'a') as fw:
+#             fw.write(f'\n\n------------ {file_name}\n\n')
+#             for line in fr:
+#                 fw.write(line)
